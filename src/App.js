92de90552f7db400c331cab0fb757c758f7aa1b0
Will Harris <will.t.harris@protonmail.com>
2020-04-16 18:02:08 -0700

    add basic fetch logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import * as moment from "moment";
 
 import WeatherCard from "./components/WeatherCard";
 
 const App = () => {
+	const [hasError, setHasErrors] = useState(false);
+	const [weather, setWeather] = useState({});
+
+	useEffect(() => {
+		//TODO ALLOW USER TO PROVIDE LAT/LONG FOR QUERY
+		const fetchData = async () => {
+			const res = await fetch(
+				`https://api.openweathermap.org/data/2.5/onecall?lat=37.773972&lon=-122.431297&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+			);
+			res
+				.json()
+				.then((res) => setWeather(res))
+				.catch((err) => setHasErrors(err));
+		};
+
+		fetchData();
+	}, []);
+
 	return (
 		<div className="flex justify-center">
 			<WeatherCard day="Thurs" icon="sun" tempFarenheit={78} tempCelcius={67} />
