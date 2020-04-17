dab2e5346c71e5d979b625b5b3938b092384e239
Will Harris <will.t.harris@protonmail.com>
2020-04-16 18:02:43 -0700

    toggle info display via state

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
+import WeatherCardHourlyInfo from "./WeatherCardHourlyInfo";
 import SunIcon from "../icons/SunIcon";
 import RainIcon from "../icons/RainIcon";
 import CloudIcon from "../icons/CloudIcon";
 
 const WeatherCard = ({ day, icon, tempFarenheit, tempCelcius }) => {
+	const [isOpen, setIsOpen] = useState(false);
+
 	const setIcon = (icon) => {
 		if (icon === "sun") {
 			return <SunIcon twClasses="mx-auto my-4 h-12 text-yellow-500" />;
@@ -15,13 +18,22 @@ const WeatherCard = ({ day, icon, tempFarenheit, tempCelcius }) => {
 		}
 	};
 	return (
-		<div className="p-4 mt-4 mx-2 border-4 border-gray-400">
-			<p className="text-center">{day}</p>
-			<div>{setIcon(icon)}</div>
-			<div>
-				<span className="pr-2 text-gray-800">{tempFarenheit}&deg;</span>
-				<span className="text-gray-500">{tempCelcius}&deg;</span>
-			</div>
+		<div
+			className="block p-4 mt-4 mx-2 border-4 border-gray-400"
+			onClick={() => setIsOpen(!isOpen)}
+		>
+			{!isOpen ? (
+				<>
+					<p className="text-center">{day}</p>
+					<div>{setIcon(icon)}</div>
+					<div>
+						<span className="pr-2 text-gray-800">{tempFarenheit}&deg;</span>
+						<span className="text-gray-500">{tempCelcius}&deg;</span>
+					</div>
+				</>
+			) : (
+				<div className="block">{isOpen && <WeatherCardHourlyInfo />}</div>
+			)}
 		</div>
 	);
 };
